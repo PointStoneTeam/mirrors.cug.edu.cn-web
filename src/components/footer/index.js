5824e70525c91ec7864d668ddc2f9fccb1ed50b1
notzheng <jianjiantsinghua@gmail.com>
2020-06-23 10:40:36 +0800

    fix: icons

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -3,14 +3,11 @@
 
 export default function Footer() {
   return (
-    <footer className="bg-white py-3 px-6">
+    <footer className="bg-white py-5 px-6">
       <div className="container mx-auto">
-        <h2 className="text-md md:text-xl">中国地质大学开源镜像站</h2>
+        {/* <h2 className="text-md md:text-xl">中国地质大学开源镜像站</h2> */}
         <div className="mt-2 text-xs md:text-sm">
-          由点石团队维护
-        </div>
-        <div className="mt-2 text-xs md:text-sm">
-          2013 - 2020
+          由点石团队维护  &copy; 2013 - 2020
         </div>
       </div>
     </footer>
