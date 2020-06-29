38e50a717ce7046b7135d8c84eb521f574009952
notzheng <jianjiantsinghua@gmail.com>
2020-06-29 10:54:59 +0800

    feat: add google analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,17 @@
+import { useEffect } from 'react'
+import Router from 'next/router'
+import * as gtag from '../lib/gtag'
 import '../styles/global.css'
 
 export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    Router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      Router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [])
   return <Component {...pageProps} />
 }
